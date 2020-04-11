367f1af6117aac183510db9651f82ac012d0c75e
Denis Žoljom <denis.zoljom@gmail.com>
2020-04-11 15:52:38 +0200

    Add view based exceptions

---
+++ src/Exception/InvalidUri.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * File containing the invalid URI exception class
+ *
+ * @since 2.0.0
+ * @package MadeByDenis\WooSoloApi\Exception
+ */
+
+declare(strict_types=1);
+
+namespace MadeByDenis\WooSoloApi\Exception;
+
+/**
+ * Class InvalidURI.
+ *
+ * Exception thrown when a URI is not valid in WordPress context.
+ *
+ * @package MadeByDenis\WooSoloApi\Exception
+ */
+final class InvalidURI extends \InvalidArgumentException implements GeneralException
+{
+	/**
+	* Create a new instance of the exception for a file that is not accessible
+	* or not readable.
+	*
+	* @param string $uri URI of the file that is not accessible or not
+	*                    readable.
+	*
+	* @return static
+	*/
+	public static function fromUri(string $uri)
+	{
+		$message = sprintf('The View URI "%s" is not accessible or readable.', $uri);
+
+		return new static($message);
+	}
+}
+
