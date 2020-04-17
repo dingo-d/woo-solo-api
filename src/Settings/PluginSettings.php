8eea3019c42015833275a949305c90de4b0c8476
Denis Žoljom <denis.zoljom@gmail.com>
2020-04-17 18:27:36 +0200

    Update register settings

--- src/Settings/PluginSettings.php
+++ src/Settings/PluginSettings.php
@@ -39,38 +39,40 @@ class PluginSettings implements Registrable
 	 */
 	public function register(): void
 	{
-		add_action('admin_init', [$this, 'registerPluginSettings']);
+		add_action('admin_init', [$this, 'registerPluginSettings'], 1);
 	}
 
 	public function registerPluginSettings()
 	{
-		register_setting('solo-api-settings-group', 'solo_api_token');
-		register_setting('solo-api-settings-group', 'solo_api_measure');
-		register_setting('solo-api-settings-group', 'solo_api_payment_type');
-		register_setting('solo-api-settings-group', 'solo_api_languages');
-		register_setting('solo-api-settings-group', 'solo_api_currency');
-		register_setting('solo-api-settings-group', 'solo_api_service_type');
-		register_setting('solo-api-settings-group', 'solo_api_show_taxes');
-		register_setting('solo-api-settings-group', 'solo_api_invoice_type');
-		register_setting('solo-api-settings-group', 'solo_api_mail_title');
-		register_setting('solo-api-settings-group', 'solo_api_message');
-		register_setting('solo-api-settings-group', 'solo_api_change_mail_from');
-		register_setting('solo-api-settings-group', 'solo_api_enable_pin');
-		register_setting('solo-api-settings-group', 'solo_api_enable_iban');
-		register_setting('solo-api-settings-group', 'solo_api_due_date');
-		register_setting('solo-api-settings-group', 'solo_api_mail_gateway');
-		register_setting('solo-api-settings-group', 'solo_api_send_pdf');
-		register_setting('solo-api-settings-group', 'solo_api_send_control');
+		$args = ['show_in_rest' => true];
+
+		register_setting('solo-api-settings-group', 'solo_api_token', $args);
+		register_setting('solo-api-settings-group', 'solo_api_measure', $args);
+		register_setting('solo-api-settings-group', 'solo_api_payment_type', $args);
+		register_setting('solo-api-settings-group', 'solo_api_languages', $args);
+		register_setting('solo-api-settings-group', 'solo_api_currency', $args);
+		register_setting('solo-api-settings-group', 'solo_api_service_type', $args);
+		register_setting('solo-api-settings-group', 'solo_api_show_taxes', $args);
+		register_setting('solo-api-settings-group', 'solo_api_invoice_type', $args);
+		register_setting('solo-api-settings-group', 'solo_api_mail_title', $args);
+		register_setting('solo-api-settings-group', 'solo_api_message', $args);
+		register_setting('solo-api-settings-group', 'solo_api_change_mail_from', $args);
+		register_setting('solo-api-settings-group', 'solo_api_enable_pin', $args);
+		register_setting('solo-api-settings-group', 'solo_api_enable_iban', $args);
+		register_setting('solo-api-settings-group', 'solo_api_due_date', $args);
+		register_setting('solo-api-settings-group', 'solo_api_mail_gateway', $args);
+		register_setting('solo-api-settings-group', 'solo_api_send_pdf', $args);
+		register_setting('solo-api-settings-group', 'solo_api_send_control', $args);
 
 		$availableGateways = $this->gateway->getAvailablePaymentGateways();
-		register_setting('solo-api-settings-group', 'solo_api_available_gateways');
+		register_setting('solo-api-settings-group', 'solo_api_available_gateways', $args);
 
 		foreach ($availableGateways as $availableGateway) {
 			$id = $availableGateway->id;
 
-			register_setting('solo-api-settings-group', 'solo_api_bill_offer-' . esc_attr($id));
-			register_setting('solo-api-settings-group', 'solo_api_fiscalization-' . esc_attr($id));
-			register_setting('solo-api-settings-group', 'solo_api_payment_type-' . esc_attr($id));
+			register_setting('solo-api-settings-group', 'solo_api_bill_offer-' . esc_attr($id), $args);
+			register_setting('solo-api-settings-group', 'solo_api_fiscalization-' . esc_attr($id), $args);
+			register_setting('solo-api-settings-group', 'solo_api_payment_type-' . esc_attr($id), $args);
 		}
 	}
 }
