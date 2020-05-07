addfa94bbc5b280901320d8ef8a52dce83a6d484
Denis Žoljom <denis.zoljom@gmail.com>
2020-05-07 13:28:50 +0200

    Update asset test

--- tests/integration/Assets/EnqueueTest.php
+++ tests/integration/Assets/EnqueueTest.php
@@ -6,6 +6,7 @@ namespace Tests\Integration\Enqueue;
 
 use Codeception\TestCase\WPTestCase;
 use IntegrationTester;
+use MadeByDenis\WooSoloApi\AdminMenus\OptionsSubmenu;
 use MadeByDenis\WooSoloApi\Assets\EnqueueResources;
 
 class EnqueueTest extends WPTestCase
@@ -15,22 +16,55 @@ class EnqueueTest extends WPTestCase
 	 */
 	protected $tester;
 
+	/**
+	 * @var mixed|\WP_UnitTest_Factory|null
+	 */
+	private $oldWpScripts;
+
 	public function setUp()
 	{
 		parent::setUp();
+
+		$this->oldWpScripts = isset($GLOBALS['wp_scripts']) ? $GLOBALS['wp_scripts'] : null;
+		remove_action('wp_default_scripts', 'wp_default_scripts');
+		remove_action('wp_default_scripts', 'wp_default_packages');
+		$GLOBALS['wp_scripts'] = new \WP_Scripts();
+		$GLOBALS['wp_scripts']->default_version = get_bloginfo('version');
 	}
 
 	public function tearDown()
 	{
+		$GLOBALS['wp_scripts'] = $this->old_wp_scripts;
+		add_action('wp_default_scripts', 'wp_default_scripts');
+
 		parent::tearDown();
 	}
-
+	
 	public function testAssetsAreEnqueued()
 	{
+		global $wp_styles, $wp_scripts;
+		set_current_screen(OptionsSubmenu::WOO_PAGE_ID);
+
 		(new EnqueueResources())->register();
 
-		set_current_screen('woocommerce_page_solo_api_options');
+		(new EnqueueResources())->enqueueScripts(OptionsSubmenu::WOO_PAGE_ID);
+		(new EnqueueResources())->enqueueStyles(OptionsSubmenu::WOO_PAGE_ID);
 
-		$scripts = get_echo('wp_print_scripts');
+		$this->assertContains(EnqueueResources::CSS_HANDLE, $wp_styles->queue);
+		$this->assertContains(EnqueueResources::JS_HANDLE, $wp_scripts->queue);
+	}
+	
+	public function testAssetsAreNotEnqueuedOnDifferentScreen()
+	{
+		global $wp_styles, $wp_scripts;
+		set_current_screen('dashboard');
+
+		(new EnqueueResources())->register();
+
+		(new EnqueueResources())->enqueueScripts('dashboard');
+		(new EnqueueResources())->enqueueStyles('dashboard');
+
+		$this->assertNotContains(EnqueueResources::CSS_HANDLE, $wp_styles->queue);
+		$this->assertNotContains(EnqueueResources::JS_HANDLE, $wp_scripts->queue);
 	}
 }
