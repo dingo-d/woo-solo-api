5d0ec0d772de23c08f30eae9e83376c54a52551d
Denis Žoljom <denis.zoljom@gmail.com>
2020-06-07 13:23:16 +0200

    Fix phpcs issues

--- tests/Integration/Activation/ActivationTest.php
+++ tests/Integration/Activation/ActivationTest.php
@@ -3,32 +3,32 @@
 declare(strict_types=1);
 
 namespace Tests\Integration\Activation;
-use Codeception\TestCase\WPTestCase;
 
-use MadeByDenis\WooSoloApi\Core\{Plugin, PluginFactory};
+use Codeception\TestCase\WPTestCase;
 use IntegrationTester;
+use MadeByDenis\WooSoloApi\Core\{Plugin, PluginFactory};
 
 class ActivationTest extends WPTestCase
 {
-    /**
-     * @var IntegrationTester
-     */
-    protected $tester;
+	/**
+	 * @var IntegrationTester
+	 */
+	protected $tester;
 
-    public function setUp()
-    {
-        parent::setUp();
-    }
+	public function setUp()
+	{
+		parent::setUp();
+	}
 
-    public function tearDown()
-    {
-        parent::tearDown();
-    }
+	public function tearDown()
+	{
+		parent::tearDown();
+	}
 
 	public function testPluginFactoryReturnsPluginInstance()
 	{
 		$plugin = PluginFactory::create();
 
 		$this->assertInstanceOf(Plugin::class, $plugin);
-    }
+	}
 }
