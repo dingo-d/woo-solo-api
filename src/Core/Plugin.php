bcd002bba2a5ebe4ff4e4953861516067f229b42
Denis Žoljom <denis.zoljom@gmail.com>
2020-07-26 18:55:39 +0200

    Initialize i18n service

--- src/Core/Plugin.php
+++ src/Core/Plugin.php
@@ -17,9 +17,10 @@ use MadeByDenis\WooSoloApi\Assets\EnqueueResources;
 use MadeByDenis\WooSoloApi\ECommerce\AdminOrder;
 use MadeByDenis\WooSoloApi\ECommerce\CheckoutFields;
 use MadeByDenis\WooSoloApi\ECommerce\WooPaymentGateways;
+use MadeByDenis\WooSoloApi\i18n\Internationalization;
+use MadeByDenis\WooSoloApi\Exception\{MissingManifest, PluginActivationFailure};
 use MadeByDenis\WooSoloApi\Rest\Endpoints\AccountDetails;
 use MadeByDenis\WooSoloApi\Settings\PluginSettings;
-use MadeByDenis\WooSoloApi\Exception\{PluginActivationFailure, MissingManifest};
 
 /**
  * Class Plugin.
@@ -32,155 +33,156 @@ use MadeByDenis\WooSoloApi\Exception\{PluginActivationFailure, MissingManifest};
 final class Plugin implements Registrable, HasActivation, HasDeactivation
 {
 
-    /**
-     * Array of instantiated services.
-     *
-     * @var array
-     */
-    private $services = [];
+	/**
+	 * Array of instantiated services.
+	 *
+	 * @var array
+	 */
+	private $services = [];
 
 	/**
-     * Activate the plugin
+	 * Activate the plugin
 	 *
 	 * @throws Exception If a condition for plugin activation isn't met.
 	 */
-    public function activate(): void
-    {
-        if (!function_exists('is_plugin_active_for_network')) {
-            include ABSPATH . '/wp-admin/includes/plugin.php';
-        }
+	public function activate(): void
+	{
+		if (!function_exists('is_plugin_active_for_network')) {
+			include ABSPATH . '/wp-admin/includes/plugin.php';
+		}
 
-        if (!class_exists('WooCommerce')) {
-          	// Deactivate the plugin.
-            deactivate_plugins(plugin_basename(__FILE__));
+		if (!class_exists('WooCommerce')) {
+			// Deactivate the plugin.
+			deactivate_plugins(plugin_basename(__FILE__));
 
-            $errorMessage = esc_html__('This plugin requires WooCommerce plugin to be active.', 'woo-solo-api');
+			$errorMessage = esc_html__('This plugin requires WooCommerce plugin to be active.', 'woo-solo-api');
 
-            throw PluginActivationFailure::activationMessage($errorMessage);
-        }
+			throw PluginActivationFailure::activationMessage($errorMessage);
+		}
 
-        $this->registerServices();
+		$this->registerServices();
 
-        // Activate that which can be activated.
-        foreach ($this->services as $service) {
-            if ($service instanceof HasActivation) {
-                $service->activate();
-            }
-        }
+		// Activate that which can be activated.
+		foreach ($this->services as $service) {
+			if ($service instanceof HasActivation) {
+				$service->activate();
+			}
+		}
 
-        \flush_rewrite_rules();
-    }
-
-	/**
-	 * Deactivate the plugin.
-	 *
-	 * @throws Exception
-	 */
-    public function deactivate(): void
-    {
-        $this->registerServices();
-
-      	// Deactivate that which can be deactivated.
-        foreach ($this->services as $service) {
-            if ($service instanceof HasDeactivation) {
-                $service->deactivate();
-            }
-        }
-
-        \flush_rewrite_rules();
-    }
-
-    /**
-     * Register the plugin with the WordPress system.
-     *
-     * The register_service method will call the register() method in every service class,
-     * which holds the actions and filters - effectively replacing the need to manually add
-     * them in one place.
-     */
-    public function register(): void
-    {
-        add_action('plugins_loaded', [$this, 'registerServices']);
-
-        $this->registerAssetsManifestData();
-    }
-
-    /**
-     * Register bundled asset manifest
-     *
-     * @throws MissingManifest Throws error if manifest is missing.
-     * @return void
-     */
-    public function registerAssetsManifestData()
-    {
-
-		$response = file_get_contents(dirname(__DIR__, 2) . '/assets/public/manifest.json');
-
-        if (! $response) {
-            $error_message = esc_html__('manifest.json is missing. Bundle the plugin before using it.', 'woo-solo-api');
-            throw MissingManifest::message($error_message);
-        }
-
-        if (! defined('ASSETS_MANIFEST')) {
-            define('ASSETS_MANIFEST', (string) $response);
-        }
-    }
+		\flush_rewrite_rules();
+	}
 
 	/**
 	 * Register the individual services of this plugin.
 	 *
 	 * @throws Exception If a service is not valid.
 	 */
-    public function registerServices(): void
-    {
-        // Bail early so we don't instantiate services twice.
-        if (! empty($this->services)) {
-            return;
-        }
+	public function registerServices(): void
+	{
+		// Bail early so we don't instantiate services twice.
+		if (!empty($this->services)) {
+			return;
+		}
 
-        static $container = null;
+		static $container = null;
 
-        if ($container === null) {
-            $container = new DiContainer();
-        }
+		if ($container === null) {
+			$container = new DiContainer();
+		}
 
-        $this->services = $container->getDiServices($this->getServiceClasses());
+		$this->services = $container->getDiServices($this->getServiceClasses());
 
-        array_walk(
-            $this->services,
-            static function ($class) {
-                if (! $class instanceof Registrable) {
-                    return;
-                }
+		array_walk(
+			$this->services,
+			static function ($class) {
+				if (!$class instanceof Registrable) {
+					return;
+				}
 
-                $class->register();
-            }
-        );
-    }
+				$class->register();
+			}
+		);
+	}
 
-    /**
-     * Get the list of services to register.
-     *
-     * A list of classes which contain hooks.
-     *
-     * @return array<int|string, array<int, string>|string> Array that contains FQCN as a key of the class to instantiate, and
-     *                      Array as a value of that key that denotes its dependencies.
-     */
-    private function getServiceClasses(): array
-    {
-        $services = [
+	/**
+	 * Deactivate the plugin.
+	 *
+	 * @throws Exception
+	 */
+	public function deactivate(): void
+	{
+		$this->registerServices();
+
+		// Deactivate that which can be deactivated.
+		foreach ($this->services as $service) {
+			if ($service instanceof HasDeactivation) {
+				$service->deactivate();
+			}
+		}
+
+		\flush_rewrite_rules();
+	}
+
+	/**
+	 * Register the plugin with the WordPress system.
+	 *
+	 * The register_service method will call the register() method in every service class,
+	 * which holds the actions and filters - effectively replacing the need to manually add
+	 * them in one place.
+	 */
+	public function register(): void
+	{
+		add_action('plugins_loaded', [$this, 'registerServices']);
+
+		$this->registerAssetsManifestData();
+	}
+
+	/**
+	 * Register bundled asset manifest
+	 *
+	 * @return void
+	 * @throws MissingManifest Throws error if manifest is missing.
+	 */
+	public function registerAssetsManifestData()
+	{
+
+		$response = file_get_contents(dirname(__DIR__, 2) . '/assets/public/manifest.json');
+
+		if (!$response) {
+			$error_message = esc_html__('manifest.json is missing. Bundle the plugin before using it.', 'woo-solo-api');
+			throw MissingManifest::message($error_message);
+		}
+
+		if (!defined('ASSETS_MANIFEST')) {
+			define('ASSETS_MANIFEST', (string)$response);
+		}
+	}
+
+	/**
+	 * Get the list of services to register.
+	 *
+	 * A list of classes which contain hooks.
+	 *
+	 * @return array<int|string, array<int, string>|string> Array that contains FQCN as a key of the class to instantiate, and
+	 *                      Array as a value of that key that denotes its dependencies.
+	 */
+	private function getServiceClasses(): array
+	{
+		$services = [
 			AccountDetails::class,
 			AdminOrder::class,
 			CheckoutFields::class,
+			EnqueueResources::class,
+			Internationalization::class,
 			OptionsSubmenu::class,
 			PluginSettings::class => [WooPaymentGateways::class],
-        	EnqueueResources::class,
-        ];
+		];
 
-        // Test mocks.
-        if (getenv('TEST') === 'true') {
-        	// Overwrite real classes.
-        }
+		// Test mocks.
+		if (getenv('TEST') === 'true') {
+			// Overwrite real classes.
+		}
 
-        return $services;
-    }
+		return $services;
+	}
 }
