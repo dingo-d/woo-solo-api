191866cb2dd8be4565740ae853b10deec35cf49c
Denis Žoljom <denis.zoljom@gmail.com>
2020-09-12 15:10:11 +0200

    Fix phpcs for unit test placeholder

--- tests/Unit/Admin/ExampleTest.php
+++ tests/Unit/Admin/ExampleTest.php
@@ -3,21 +3,23 @@
 namespace Tests\Unit\Admin;
 
 use Codeception\Test\Unit;
-
 use Brain\Monkey;
 
-class ExampleTest extends Unit {
+class ExampleTest extends Unit
+{
 
-	protected function _before() {
-        Monkey\setUp();
-    }
+	protected function _before() // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+	{
+		Monkey\setUp();
+	}
 
-    protected function _after() {
+	protected function _after() // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
+	{
 		Monkey\tearDown();
-    }
+	}
 
 	public function testExample()
 	{
 		$this->assertTrue(true, 'Assertion is not true');
-    }
+	}
 }
