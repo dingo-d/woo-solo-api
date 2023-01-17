f495aa21e72f67cc17f6b8272be3735b48e76ce9
Denis Žoljom <denis.zoljom@gmail.com>
2023-01-17 21:08:25 +0100

    Add translation to invoice type const

--- assets/dev/scripts/const/invoiceType.jsx
+++ assets/dev/scripts/const/invoiceType.jsx
@@ -4,6 +4,6 @@ export const invoiceType = [
 	{value: '1', label: 'R'},
 	{value: '2', label: 'R1'},
 	{value: '3', label: 'R2'},
-	{value: '4', label: 'No label'},
-	{value: '5', label: 'In advance'},
+	{value: '4', label: __('No label', 'woo-solo-api')},
+	{value: '5', label: __('In advance', 'woo-solo-api')},
 ];
