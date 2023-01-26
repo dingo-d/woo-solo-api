33a11a7bfeb811a85a6eee6a5bbae46a75444922
Denis Žoljom <dingo-d@users.noreply.github.com>
2023-01-26 15:01:42 +0100

    Update tests

--- tests/Integration/Core/PluginTest.php
+++ tests/Integration/Core/PluginTest.php
@@ -4,27 +4,59 @@ namespace Tests\Integration\Core;
 
 use MadeByDenis\WooSoloApi\Core\PluginFactory;
 use MadeByDenis\WooSoloApi\Utils\FetchExchangeRate;
+use Mockery;
+use WP_Upgrader;
 use Yoast\WPTestUtils\WPIntegration\TestCase;
 
 uses(TestCase::class);
 
 beforeEach(function () {
 	parent::setUp();
-});
+	delete_transient(FetchExchangeRate::TRANSIENT);
 
-afterEach(function () {
-	parent::tearDown();
-});
-
-test('Plugin upgrader will exit early in the case the action is not update', function () {
+	$this->upgradeMock = Mockery::mock(WP_Upgrader::class);
 	/*
 	 * Namespace and PSR-4 prefixes shouldn't matter,
 	 * as the instance of the plugin is already set during test initialization.
 	 */
-	$pluginInstance = PluginFactory::create(['test' => ['test']], '\\WooSoloApi\\Test');
+	$this->pluginInstance = PluginFactory::create(['test' => ['test']], '\\WooSoloApi\\Test');
+});
 
+afterEach(function () {
+	$this->upgradeMock = null;
+	$this->pluginInstance = null;
+	delete_transient(FetchExchangeRate::TRANSIENT);
+
+	parent::tearDown();
+});
+
+test('Plugin upgrader will exit early in the case the action is not update', function () {
 	$hookExtra = [
 		'action' => 'delete',
+		'type' => 'plugin',
+		'bulk' => false,
+		'plugins' => [
+			'woocommerce/woocommerce.php',
+			'woo-solo-api/woo-solo-api.php',
+		],
+		'themes' => ['twentytwenty'],
+		'translations' => [
+			'language' => 'en-En',
+			'type' => 'plugin',
+			'slug' => 'woo-solo-api',
+			'version' => '3.0.0',
+		],
+	];
+
+	$this->pluginInstance->updatePluginActions($this->upgradeMock, $hookExtra);
+	$transient = get_transient(FetchExchangeRate::TRANSIENT);
+
+	expect($transient)->toBeEmpty();
+});
+
+test('Plugin upgrader will exit early in the case the type is not plugin', function () {
+	$hookExtra = [
+		'action' => 'update',
 		'type' => 'theme',
 		'bulk' => false,
 		'plugins' => [
@@ -40,7 +72,61 @@ test('Plugin upgrader will exit early in the case the action is not update', fun
 		],
 	];
 
-	$pluginInstance->updatePluginActions(new \WP_Upgrader(), $hookExtra);
+	$this->pluginInstance->updatePluginActions($this->upgradeMock, $hookExtra);
+	$transient = get_transient(FetchExchangeRate::TRANSIENT);
+
+	expect($transient)->toBeEmpty();
+});
+
+test('Plugin upgrader won\'t do anything in the case our plugin is not in the list', function () {
+	$hookExtra = [
+		'action' => 'update',
+		'type' => 'plugin',
+		'bulk' => false,
+		'plugins' => [
+			'woocommerce/woocommerce.php',
+		],
+		'themes' => ['twentytwenty'],
+		'translations' => [
+			'language' => 'en-En',
+			'type' => 'plugin',
+			'slug' => 'woo-solo-api',
+			'version' => '3.0.0',
+		],
+	];
+
+	$this->pluginInstance->updatePluginActions($this->upgradeMock, $hookExtra);
+	$transient = get_transient(FetchExchangeRate::TRANSIENT);
+
+	expect($transient)->toBeEmpty();
+});
+
+test('Plugin upgrader will delete the transient', function () {
+	set_transient(FetchExchangeRate::TRANSIENT, 'Testing transient!');
+	$transient = get_transient(FetchExchangeRate::TRANSIENT);
+
+	expect($transient)
+		->not->toBeEmpty()
+		->toBe('Testing transient!');
+
+	$hookExtra = [
+		'action' => 'update',
+		'type' => 'plugin',
+		'bulk' => false,
+		'plugins' => [
+			'woocommerce/woocommerce.php',
+			'woo-solo-api/woo-solo-api.php',
+		],
+		'themes' => ['twentytwenty'],
+		'translations' => [
+			'language' => 'en-En',
+			'type' => 'plugin',
+			'slug' => 'woo-solo-api',
+			'version' => '3.0.0',
+		],
+	];
+
+	$this->pluginInstance->updatePluginActions($this->upgradeMock, $hookExtra);
 	$transient = get_transient(FetchExchangeRate::TRANSIENT);
 
 	expect($transient)->toBeEmpty();
